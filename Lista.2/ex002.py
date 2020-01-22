fee310c8b98198bff6cca01187b7ad74d9fb174b
Arthur Xavier <arthxvr@outlook.com>
2020-01-22 18:46:34 -0300

    Adiciona exercícios Altera linhas 4 e 6 do exercício ex002 da lista2 do curso Python para Zumbis

--- Lista.2/ex002.py
+++ Lista.2/ex002.py
@@ -1,6 +1,6 @@
 ano = int(input('Digite o ano: '))
 
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print('O ano digitado É BISSEXTO.')
+    print(f'O ano {ano} É BISSEXTO.')
 else:
-    print('O ano digitado NÃO É BISSEXTO.')
+    print(f'O ano {ano} NÃO É BISSEXTO.')
